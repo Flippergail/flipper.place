a47b28cdc4615b365ad30f6546cbd6dd581a40dc
Flippergail <ticarter11@icloud.com>
2023-02-13 17:33:54 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import HomePage from "/Components/HomePage/HomePage"
-import NavBar from '@/Components/HomePage/NavBar'
+import NavBar from '/Components/HomePage/NavBar'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Flipper's Portfolio</title>
-        <meta name="description" content="Flippergail's official portfolio" />
+        <meta name="description" content="Flippergail&apos;s official portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/Images/FlipperPFP.png" />
       </Head>
