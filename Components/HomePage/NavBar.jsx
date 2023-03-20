0131a93676b167f74744cb378d72928d2841370c
Flippergail <ticarter11@icloud.com>
2023-03-20 19:37:22 +0000

    slight change to remove dot from lists

--- Components/HomePage/NavBar.jsx
+++ Components/HomePage/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function NavBar() {
     return(
-        <header className="p-4 justify-between items-center flex h-[10%] fixed w-full z-50">
+        <header className="list-none p-4 justify-between items-center flex h-[10%] fixed w-full z-50">
             <li className="relative h-[120%] aspect-square">
                 <Image
                     src="/Images/FlipperPFP.png"
@@ -15,7 +15,7 @@ export default function NavBar() {
             <li className="self-end" ><p className="text-text text-4xl font-semibold">flipper.place</p></li>
 
             <nav>
-                <ul className="flex gap-3 relative">
+                <ul className="list-none flex gap-3 relative">
                     <li className="group">
                         <a className="text-text text-l  font-semibold" href="#Home">Home</a>
                         <div className="rounded-full ml-[20%] mr-[20%] flex-grow h-0.5 group-hover:bg-text"/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./Components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
