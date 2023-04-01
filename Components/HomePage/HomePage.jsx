975a6e65afbc24e256459a29927a26585ec4898e
Flippergail <ticarter11@icloud.com>
2023-04-01 12:18:58 +0100

    updating styling to improve it for mobile, need to still add burger nav bar for mobile

--- Components/HomePage/HomePage.jsx
+++ Components/HomePage/HomePage.jsx
@@ -3,21 +3,25 @@ import React from "react";
 
 export default function HomePage() {
     return(
-        <div className="relative h-screen w-full flex flex-row align-middle" id="Home">
+        <div className="relative h-screen w-full flex flex-col md:flex-row align-middle" id="Home">
             <Image
                 src="/Images/HomePageBG.png"
                 alt="Background Image"
                 fill
-                className="-z-10"
+                className="-z-10 object-cover"
+                priority={true}
             />
             
-            <p className="absolute text-text text-5xl leading-tight font-semibold mt-[11%] ml-[12%]">Hello, my name is<br/>Tristan.<br/><br/>I am a Full Stack<br/>Developer living in<br/>the UK.</p>
-            <div className="relative h-[70%] aspect-[791/597] ml-[42%]" >
+            <p className=" ml-12 lg:ml-48 mt-24 lg:mt-44 text-text text-4xl md:text-3xl lg:text-5xl leading-tight font-semibold">Hello, my name is<br/>Tristan.<br/><br/>I am a Full Stack<br/>Developer living in<br/>the UK.</p>
+
+            <div className="relative lg:mt-24 overflow-hidden">
                 <Image
                     src="/Images/LaptopGrad.png"
                     alt="Laptop Gradient Vector"
-                    fill
-                    className="z-0 mt-[14%]"
+                    height={513}
+                    width={672}
+                    className="z-0"
+                    priority={true}
                 />
             </div>
         </div>
--- Components/HomePage/NavBar.jsx
+++ Components/HomePage/NavBar.jsx
@@ -1,35 +1,42 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+      setIsOpen(!isOpen)
+    }
     return(
-        <header className="list-none p-4 justify-between items-center flex h-[10%] fixed w-full z-50">
-            <li className="relative h-[120%] aspect-square">
+        <div className="p-4 justify-between items-center flex flex-row h-[4.5rem] md:h-13 lg:h-[4.5rem] fixed w-full z-50 backdrop-blur-md">
+            <div className="relative hover:animate-spin">
                 <Image
                     src="/Images/FlipperPFP.png"
                     alt="PFP"
-                    fill
+                    width={64}
+                    height={64}
+                    priority={true}
                 />
-            </li>
+            </div>
 
-            <li className="self-end" ><p className="text-text text-4xl font-semibold">flipper.place</p></li>
-
+            <p className="md:self-center text-text text-3xl md:text-5xl font-semibold">flipper.place</p>
+            
             <nav>
                 <ul className="list-none flex gap-3 relative">
                     <li className="group">
-                        <a className="text-text text-l  font-semibold" href="#Home">Home</a>
+                        <a className="text-text text-l font-semibold" href="#Home">Home</a>
                         <div className="rounded-full ml-[20%] mr-[20%] flex-grow h-0.5 group-hover:bg-text"/>
                     </li>
                     <li className="group">
-                        <a className="text-text text-l  font-semibold" href="#Projects">Projects</a>
+                        <a className="text-text text-l font-semibold" href="#Projects">Projects</a>
                         <div className="rounded-full ml-[20%] mr-[20%] flex-grow h-0.5 group-hover:bg-text"/>
                     </li>
                     <li className="group">
-                        <a className="text-text text-l  font-semibold" href="#About">About</a>
+                        <a className="text-text text-l font-semibold" href="#About">About</a>
                         <div className="rounded-full ml-[20%] mr-[20%] flex-grow h-0.5 group-hover:bg-text"/>
                     </li>
                 </ul>
             </nav>
-        </header>
+        </div>
     )
 }
