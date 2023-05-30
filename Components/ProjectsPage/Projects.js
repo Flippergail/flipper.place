c311007bf1ad444a8251c0f71ab6f93a2e51d3f9
Flippergail <ticarter11@icloud.com>
2023-05-30 10:24:03 +0100

    Adding TeachMe to Projects

--- Components/ProjectsPage/Projects.js
+++ Components/ProjectsPage/Projects.js
@@ -1,5 +1,14 @@
 const Projects = [
     {
+        title: "TeachMe",
+        description: "A mobile app for revision and learning. Made using React Native. Currently in development.",
+        detailedDesc: "",
+        image: "/Images/TeachMe.png",
+        link: "https://github.com/Flippergail/TeachMe-React-Native-JS",
+        imageWidth: 1230,
+        imageHeight: 691,
+    },
+    {
         title: "C++ Chess Engine",
         description: "A chess engine written in C++ with no external libraries from scratch and 2,224 lines of code.",
         detailedDesc: "A chess engine written in C++ with no external libraries from scratch and 2,224 lines of code that uses the Minimax algorithm with alpha-beta pruning to determine the best move for the AI.",
@@ -14,15 +23,6 @@ const Projects = [
         detailedDesc: "A 2d game in the style of google dinausor. Made using Next.js + Tailwind and react-three-fiber for the actual game part of the website. I made the game as a light hearted entry to a school STEM competition which went on to win and have almost 1000 different people play. Being built to work well on both desktop and mobile.",
         image: "/Images/SnellRun.png",
         link: "https://snellrun.com/",
-        imageWidth: 1200,
-        imageHeight: 613,
-    },
-    {
-        title: "TeachMe",
-        description: "A mobile app for revision and learning. Made using React Native. Currently in development.",
-        detailedDesc: "",
-        image: "/Images/TeachMe.png",
-        link: "https://github.com/Flippergail/TeachMe-React-Native-JS",
         imageWidth: 1200,
         imageHeight: 613,
     },
