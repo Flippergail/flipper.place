05070fbd4ac9252c6ab1bba427d09d8b9fb59c5f
Tristan <ticarter11@icloud.com>
2025-06-15 19:18:51 +0100

    Update HomePage.jsx

--- Components/HomePage/HomePage.jsx
+++ Components/HomePage/HomePage.jsx
@@ -20,7 +20,7 @@ export default function HomePage() {
                 priority
             />*/}
             {/*<canvas className="absolute" id="gradient-canvas" data-transition-in />*/}
-            <p className="hidden">Tristan Carter is an aspiring software engineer currently living in the UK. To find out more go to either his wesbsite, Github profile or LinkedIn</p>
+            <p className="hidden">Official portfolio of Tristan Carter, developed in Next.js, an aspiring software engineer currently living in the UK. To find out more go to either his wesbsite, Github profile or LinkedIn</p>
             <p className="z-10 pt-6 lg:pt-0 text-text text-4xl md:hidden lg:block lg:text-5xl leading-tight font-semibold">Hello, my name is<br/>Tristan Carter.<br/><br/>I am an aspiring software engineer<br/>currently living in the UK.</p>
             
             <p className="z-10 pt-8 hidden md:block lg:hidden text-text text-2xl leading-tight font-semibold">Hello, my name is Tristan Carter.<br/>I am a an aspiring software engineer currently <br/> living in the UK.</p>
